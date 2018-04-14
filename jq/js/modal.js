f44e768306ddf28c29aa795028c99f7d0884b7da
Wuyk <wuykjiu@163.com>
2018-04-15 02:11:52 +0800

    Site updated: 2018-04-15 02:11:35

---
+++ jq/js/modal.js
@@ -0,0 +1,39 @@
+let modal = (function () {
+    let $window  = $(window),
+        $modal   = $('<div class="modal">'),
+        $content = $('<div class="modal-content">'),
+        $close   = $('<button role="button" class="modal-close">close</button>');
+
+    $modal.append($content, $close);
+
+    $close.on('click', function(e){
+        e.preventDefault();
+        modal.close();
+    });
+
+    return {
+        center: function() {                        
+            var top  = Math.max($window.height() - $modal.outerHeight(), 0) / 2;
+            var left = Math.max($window.width() - $modal.outerWidth(), 0) / 2;
+            $modal.css({                                
+                top:top + $window.scrollTop(),            
+                left:left + $window.scrollLeft()          
+            });
+    },
+        open: function(settings) {
+            $content.empty().append(settings.content);
+            $modal.css({
+                width : settings.width || 'auto',
+                height: settings.height || 'auto'
+            }).fadeIn(700).appendTo('body');
+
+            modal.center();
+            $(window).on('resize', modal.center);
+        },
+        close: function() {
+            $content.empty();
+            $modal.detach();
+            $(window).off('resize', modal.center);
+        }
+    };
+}());</div></div>
