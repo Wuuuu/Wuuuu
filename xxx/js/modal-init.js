5e1705c1a286bf113648090fa82f6d704f00b54d
Wuyk <wuykjiu@163.com>
2018-04-15 01:51:02 +0800

    Site updated: 2018-04-15 01:50:55

--- xxx/js/modal-init.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  let $content = $('#share-options').detach();   // 删除modal-options,但是保存副本
-
-  $('#share').on('click', function() {           // 点击打开Modal
-    modal.open({content: $content, 
-                width  : 300, 
-                height : 200});
-  });
-}());
